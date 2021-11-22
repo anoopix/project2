e1737ced886335fd603bc1beea21c5ad9f7b6501
axr5239 <axr5239@rit.edu>
2021-11-21 19:17:51 -0500

    Added csrf

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
 const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -65,7 +66,17 @@ app.use(session({
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+
+app.disable('x-powered-by');
 app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
